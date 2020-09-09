90aeecb3f8ed3908c2f082deb0720fea07e4eb76
EC2 Default User <ec2-user@ip-172-31-77-14.ec2.internal>
2020-09-09 09:39:00 +0000

    [Finish] 1-C6

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -19,6 +19,13 @@ class BlogsController < ApplicationController
   end
 
   def edit
+    @blog = Blog.find(params[:id]) #findでDBから投稿データを探す
+  end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
   end
 
   private # ストロングパラメータ
