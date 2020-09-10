e5d61802654580f0c5d70ad4486044c815318b45
EC2 Default User <ec2-user@ip-172-31-77-14.ec2.internal>
2020-09-10 02:15:11 +0000

    [Finish] 1-C7

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -28,6 +28,12 @@ class BlogsController < ApplicationController
     redirect_to blog_path(blog)
   end
 
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
+
   private # ストロングパラメータ
   def blog_params #blog_paramsの中にフォームで入力されたデータが格納
     params.require(:blog).permit(:title, :category, :body)
